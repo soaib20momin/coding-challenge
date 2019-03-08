c15afe3ee5858fd4188111ea295060971b0ef28b
Soaib Momin <smomin20@gmail.com>
2019-03-08 01:54:34 -0500

    Created User class for Authentication

--- config/bundles.php
+++ config/bundles.php
@@ -11,7 +11,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
